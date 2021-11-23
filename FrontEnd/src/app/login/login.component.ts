78574af95c6fa9b41ee0b19ac88bbca789fc9df6
fazi1live <faziislive@gmail.com>
2021-11-24 03:01:01 +0500

    comit

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  _LoginForm:FormGroup | any;
+  constructor(private _FormBuilder:FormBuilder) { this.LoginFormModel() }
 
-  constructor() { }
-
+  LoginFormModel(){
+    this._LoginForm = this._FormBuilder.group({
+      Email:[''],
+      Password:['']
+    }) 
+  }
   ngOnInit(): void {
+  }
+
+  LogIn(){
+    const _LoginFormValues = this._LoginForm.value;
+    console.log(_LoginFormValues);
   }
 
 }
