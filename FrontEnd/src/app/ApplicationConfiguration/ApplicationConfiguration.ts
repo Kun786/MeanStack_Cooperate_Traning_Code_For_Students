cd49377623591093d84d12b1acc7727ce0bd546b
fazi1live <faziislive@gmail.com>
2021-11-27 18:53:16 +0500

    save changes

--- FrontEnd/src/app/ApplicationConfiguration/ApplicationConfiguration.ts
+++ FrontEnd/src/app/ApplicationConfiguration/ApplicationConfiguration.ts
@@ -2,14 +2,14 @@
 export const baseUrl = 'http://localhost:1234';
 
 //Routes
-export const UserUrl = baseUrl+'/TestingAndLearningRoute/testing';
-export const GetUserUrl = baseUrl+'/TestingAndLearningRoute/GetAllUsers';
-export const DeleteUserUrl = baseUrl+'/TestingAndLearningRoute/DeleteUserById/'
-export const LoginUserUrl = baseUrl+'/TestingAndLearningRoute/LoginUser';
-export const SendMessageToAdminUrl = baseUrl+'/ChatManagement/SendMessageToAdmin';
-export const SendMessageToUserUrl = baseUrl+'/ChatManagement/SendMessageToUser';
-export const GetMessageFromAdminUrl = baseUrl+'/ChatManagement/GetMessageFromAdmin';
-export const GetMessageFromUserUrl = baseUrl+'/ChatManagement/GetMessageFromUser';
-export const GetUserByIdUrl = baseUrl+'/TestingAndLearningRoute/GetUserById/'
-export const UpdateByIdUrl = baseUrl+'/TestingAndLearningRoute/UpdateUserById/';
+export const UserUrl = '/TestingAndLearningRoute/testing';
+export const GetUserUrl = '/TestingAndLearningRoute/GetAllUsers';
+export const DeleteUserUrl = '/TestingAndLearningRoute/DeleteUserById/'
+export const LoginUserUrl = '/TestingAndLearningRoute/LoginUser';
+export const SendMessageToAdminUrl = '/ChatManagement/SendMessageToAdmin';
+export const SendMessageToUserUrl = '/ChatManagement/SendMessageToUser';
+export const GetMessageFromAdminUrl = '/ChatManagement/GetMessageFromAdmin';
+export const GetMessageFromUserUrl = '/ChatManagement/GetMessageFromUser';
+export const GetUserByIdUrl = '/TestingAndLearningRoute/GetUserById/'
+export const UpdateByIdUrl = '/TestingAndLearningRoute/UpdateUserById/';
 //Routes
